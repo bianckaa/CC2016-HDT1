7b02654a730456f96541142a888af71558b4e44f
bianckaa <biancka30ar@gmail.com>
2025-01-21 15:26:26 -0600

    crear metodos OsterBlender

--- OsterBlender.java
+++ OsterBlender.java
@@ -1,3 +1,33 @@
-public class OsterBlender {
-    
+public class OsterBlender implements IBlender{
+    private int velocidad = 0;
+
+    @Override
+    public void SpeedUp(){
+        velocidad++;
+    }
+
+    @Override
+    public boolean IsFull(){
+        
+    }
+
+    @Override
+    public void Fill(){
+
+    }
+
+    @Override
+    public void Empty(){
+
+    }
+
+    @Override
+    public void SpeedDown (){
+        velocidad--;
+    }
+
+    @Override
+    public int GetSpeed(){
+        
+    }
 }
