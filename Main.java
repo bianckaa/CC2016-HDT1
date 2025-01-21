661e01b40513e2d71be3034cec81fc5ee10d36eb
Diana-Sosa18 <sosadiana364@gmail.com>
2025-01-21 16:17:16 -0600

    menu

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-    Holagit commit 
-    bcncncnc
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.println("--OPCIONES DE LA LICUADORA--");
+    System.out.println("1. Subir velocidad");
+    System.out.println("2. Bajar velocidad");
+    System.out.println("3. Llenar licuadora");
+    System.out.println("4. Vaciar licuadora");
+    System.out.println("5. ¿Licuadora llena si/no?");
+    System.out.println("Por favor escoja una opcion");
+    
     
 }
