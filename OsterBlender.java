df09f55c76f5c2e04c0c29f5aa287d74d14f5af4
bianckaa <biancka30ar@gmail.com>
2025-01-22 21:29:51 -0600

    documentacion 2 OsterBlender

--- OsterBlender.java
+++ OsterBlender.java
@@ -9,24 +9,47 @@
  * 
  * 
  * Clase: OsterBlender
- * Clase que implementa la interfaz IBlender.
+ * Clase que implementa la interfaz IBlender y representa una licuadora de marca Oster.
  */
 public class OsterBlender implements IBlender{
 
     private int velocidad;
     private boolean lleno;
 
+    /**
+     * Constructor para inicializar la licuadora con una determinada velocidad y estado de llenado.
+     * 
+     * @param velocidad  velocidad que tiene la licuadora al encenderse.
+     * @param lleno estado inical de llenado en el que se encuentra la licuadora al encenderse (true si está llena, false si está vacía).
+     */
     public OsterBlender(int velocidad, boolean lleno) {
         this.velocidad = 0;
         this.lleno = false;
     }
 
+    /**
+     * Establece la velocidad de la licuadora.
+     * 
+     */
     public void setVelocidad(int velocidad) { this.velocidad = velocidad;}
     
+
+    /**
+     * Establece el estado de llenado en el que se encuentra la licuadora.
+     * 
+     */
     public void setLleno(boolean lleno) { this.lleno = lleno;}
 
+    /**
+     * Obtiene la velocidad actual de la licuadora.
+     * 
+     */
     public int getVelocidad() { return this.velocidad; }
 
+    /**
+     * Obtiene el estado de llenado de la licuadora.
+     * 
+     */
     public boolean getLleno() { return this.lleno; }
 
     /**
